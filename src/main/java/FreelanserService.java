5dfe414de3abd61668bdfa064ae8318bb56c80aa
Neto test <sysl86@mail.ru>
2023-06-11 17:33:35 +0400

    Reformate code commit

--- src/main/java/FreelanserService.java
+++ src/main/java/FreelanserService.java
@@ -1,22 +1,22 @@
 public class FreelanserService {
-public int calculate ( int income, int expense, int threshold){
-    int money = 0;
+    public int calculate(int income, int expense, int threshold) {
+        int money = 0;
 
-    int monthsRest = 0;
+        int monthsRest = 0;
 
-    for (int i = 0; i < 12; i++) {
-        if (money >= threshold){
-            // отдыхаем
-            money -= expense;
-            money /= 3;
-            monthsRest ++;
-        } else {
-            // работаем
-            money += income;
-            money -= expense;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                // отдыхаем
+                money -= expense;
+                money /= 3;
+                monthsRest++;
+            } else {
+                // работаем
+                money += income;
+                money -= expense;
+            }
+
         }
-        
+        return monthsRest;
     }
-    return monthsRest;
-}
 }
--- src/test/java/FreelanserServiceTest.java
+++ src/test/java/FreelanserServiceTest.java
@@ -3,21 +3,22 @@ import org.junit.jupiter.api.Test;
 
 public class FreelanserServiceTest {
     @Test
-public void testCalculate(){
+    public void testCalculate() {
         FreelanserService service = new FreelanserService();
 
         int actual = service.calculate(10_000, 30_000, 20_000);
         int expected = 3;
 
-        Assertions.assertEquals(expected , actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void testCalculate2(){
+    public void testCalculate2() {
         FreelanserService service = new FreelanserService();
 
         int actual = service.calculate(100_000, 60_000, 150_000);
         int expected = 2;
 
-        Assertions.assertEquals(expected , actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
